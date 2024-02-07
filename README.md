1904cb2bc160ce6521105864db05a95282510d23
kalanakt <e19198@eng.pdn.ac.lk>
2024-02-07 14:55:23 +0530

    initiate website

---
+++ README.md
@@ -0,0 +1,21 @@
+```bash
+git clone git@github.com:kalanakt/website.git
+```
+
+```bash
+cd website
+```
+
+```bash
+npm install
+```
+
+```bash
+npm run dev
+```
+
+If you want to create a new post, run:
+
+```bash
+npm run write
+```
---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export const SITE = {
+  title: "KalanaKt - Kalana Kithmina",
+  website: "https://kalanakt.vercel.com",
+  description:
+    "Thoughts. Learning about math and programming. Currently working as a Software Engineer at Netronk Innovations",
+  author: "Kalana Kithmina",
+};
+
+export const GITHUB_REPO_URL = "https://github.com/kalanakt/";
+export const LINKEDIN_URL = "https://linkedin.com/in/kalanakt";
