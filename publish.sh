4cb4e5099fe39cc925e641d2bcbf8b61fa2f1680
kalanakt <e19198@eng.pdn.ac.lk>
2024-02-27 18:50:35 +0530

    modify:bash-script::echo-colors

--- publish.sh
+++ publish.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-echo "Adding all changes to the staging area..."
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "${GREEN}Adding all changes to the staging area...${NC}"
 git add .
 
-echo "Committing changes with message: $1"
+echo -e "${GREEN}Committing changes with message: $1${NC}"
 git commit -m "$1"
 
-echo "Pushing changes to the remote repository..."
+echo -e "${GREEN}Pushing changes to the remote repository...${NC}"
 git push origin main
 
-echo "Deploying to production using Vercel..."
+echo -e "${GREEN}Deploying to production using Vercel...${NC}"
 vercel --prod
 
-echo "Done!"
+echo -e "${GREEN}Done!${NC}"
+
